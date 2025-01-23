018a54943d3c007f4709b3ebec806ac1b0a980a6
Bhavesh Mankar <bhaveshmankar10@gmail.com>
2025-01-24 00:45:51 +0530

    initial setup

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ function updateTotal() {
 
   const balanceTotal = incomeTotal - expenseTotal;
 
-  balance.textContent = formatter.format(balanceTotal).substring(1);
+  balance.textContent = formatter.format(balanceTotal);
   income.textContent = formatter.format(incomeTotal);
-  expense.textContent = formatter.format(expenseTotal * -1);
+  expense.textContent = formatter.format(expenseTotal);
 }
 
 function renderList() {
@@ -42,7 +42,6 @@ function renderList() {
 
   transactions.forEach(({ id, name, amount, date, type }) => {
     const sign = "income" === type ? 1 : -1;
-
     const li = document.createElement("li");
 
     li.innerHTML = `
@@ -50,11 +49,9 @@ function renderList() {
         <h4>${name}</h4>
         <p>${new Date(date).toLocaleDateString()}</p>
       </div>
-
       <div class="amount ${type}">
         <span>${formatter.format(amount * sign)}</span>
       </div>
-    
       <div class="action">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" onclick="deleteTransaction(${id})">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
